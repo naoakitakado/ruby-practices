2a9feb33156a75ca67dc1343137aef1d4bb0d97c
naoakitakado <naoakitakado666@gmail.com>
2022-09-14 09:14:53 +0900

    レビューを踏まえての修正対応

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -16,9 +16,8 @@ last_date = Date.new(year, month, -1)
 first_date = Date.new(year, month, 1)
 
 SATURDAY = 6
-ONEWEEK = 7
-LASTDATE = last_date.day
-week = []
+DAYS_OF_WEEK = 7
+week = ["  "] * first_date.wday
 
 puts "     #{month}月 #{year}    "
 puts "日 月 火 水 木 金 土"
@@ -26,16 +25,11 @@ puts "日 月 火 水 木 金 土"
 (first_date..last_date).each do |date|
   one_day = date.day
   one_day_of_week = date.wday
-  one_day_shaped = one_day.to_s.rjust(2)
-  week << one_day_shaped
+  week << one_day.to_s.rjust(2)
 
   if one_day_of_week == SATURDAY
-    if week.length != ONEWEEK
-      blank_times = ONEWEEK - week.length
-      blank_times.times{ week.unshift("  ") }
-    end
     puts week.join(" ")
     week = []
   end
-  puts week.join(" ") if one_day == LASTDATE
 end
+puts week.join(" ")
