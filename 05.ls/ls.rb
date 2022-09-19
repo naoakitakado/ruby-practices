f57e0e88ca672ac4184418ca1905a30e4c1197c0
naoakitakado <naoakitakado666@gmail.com>
2022-09-19 11:25:01 +0900

    ペアプロを踏まえ修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -11,20 +11,10 @@ end
 def lineshaping(row_number, items)
   ascending_items = items.sort
   items_cut = ascending_items.each_slice(row_number).to_a
-  max = items_cut.max_by(&:size).size
-
-  if items_cut.count == 1
-    test = items_cut.flatten.map do |o|
-      o.tr(' ', '')
-    end
-    puts test.join('   ')
-    exit
-  else
-    items_cut.each do |a|
-      a << '' while a.size < max
-    end
-    items_cut.transpose
+  items_cut.each do |item_cut|
+    item_cut << '' while item_cut.size < row_number
   end
+  items_cut.transpose
 end
 
 max = 0
@@ -38,4 +28,7 @@ end
 
 items = dir_items.map { |item| item.ljust(max) }
 
-lineoutput(DISPLAY_ROW_NUMBER, items)
+row_number = items.length / DISPLAY_ROW_NUMBER
+row_number += 1 unless (items.length % DISPLAY_ROW_NUMBER).zero?
+
+lineoutput(row_number, items)
